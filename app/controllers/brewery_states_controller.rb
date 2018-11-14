ee354cca844e6190730ddf8378141d7a1b57e0a9
Xplor8r <mpieschel39@gmail.com>
2018-11-14 15:26:08 -0800

    states controller and routes

--- app/controllers/brewery_states_controller.rb
+++ app/controllers/brewery_states_controller.rb
@@ -1,2 +1,16 @@
 class BreweryStatesController < ApplicationController
+    before_action :state
+
+    def index
+      @brewery_threads = BreweryThread.where(brewery_state: @brewery_state) if @brewery_state.present?
+      @brewery_threads = @brewery_threads.updated_at.sorted.includes(:user, :brewery_state)
+      render "brewery_threads/index"
+    end
+  
+    private
+      def state
+        @brewery_state = BreweryState.friendly.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        redirect_to brewery_threads_path
+      end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   get 'brewery_threads/new'
   get 'brewery_threads/show'
   get 'posts/edit'
+  resources :brewery_threads, path: :threads do
+    get "brewery_state/:id", to: "brewery_states#index", as: :brewery_state
+    resources :posts, path: :posts
+  end
+  root to: "brewery_threads#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
