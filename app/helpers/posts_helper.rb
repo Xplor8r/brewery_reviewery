96ab7a8d1d0bdee8f5a70f0c91a5b0f4ab34c18c
Xplor8r <mpieschel39@gmail.com>
2018-11-15 14:50:18 -0800

    posts helpers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -3,7 +3,7 @@ module PostsHelper
        gravatar_image_tag(email, gravatar: { size: 40 }, class: "rounded avatar")
     end
     
-    def state_link(state)
+    def brewery_state_link(state)
        link_to state.name, brewery_state_brewery_threads_path(state)
     end
 end
