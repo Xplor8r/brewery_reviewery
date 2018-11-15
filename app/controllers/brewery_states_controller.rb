83b8dbe2e2b53058428b9415a2e6d627254e2db5
Xplor8r <mpieschel39@gmail.com>
2018-11-15 14:47:44 -0800

    sessions and update states and users controller

--- app/controllers/brewery_states_controller.rb
+++ app/controllers/brewery_states_controller.rb
@@ -1,14 +1,14 @@
 class BreweryStatesController < ApplicationController
-    before_action :state
+    before_action :b_state
 
     def index
       @brewery_threads = BreweryThread.where(brewery_state: @brewery_state) if @brewery_state.present?
-      @brewery_threads = @brewery_threads.updated_at.sorted.includes(:user, :brewery_state)
+      @brewery_threads = @brewery_threads.sorted.includes(:user, :brewery_state)
       render "brewery_threads/index"
     end
   
     private
-      def state
+      def b_state
         @brewery_state = BreweryState.friendly.find(params[:id])
       rescue ActiveRecord::RecordNotFound
         redirect_to brewery_threads_path
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  #get 'users/edit'
-  #get 'users/new'
-  #get 'users/show'
-  #get 'brewery_threads/edit'
-  #get 'brewery_threads/index'
-  #get 'brewery_threads/new'
-  #get 'brewery_threads/show'
-  #get 'posts/edit'
+  get "/signin", to: "sessions#new"
+  post "/sessions/create", to: "sessions#create"
+  delete "/signout", to: "sessions#destroy"
   resources :users
   resources :brewery_threads, path: :threads do
     collection do
@@ -17,5 +12,4 @@ Rails.application.routes.draw do
     resources :posts, path: :posts
   end
   root to: "brewery_threads#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
