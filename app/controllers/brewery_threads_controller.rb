1579e87606833ef93c9e56a96f12abec4c168052
Xplor8r <mpieschel39@gmail.com>
2018-11-15 13:58:58 -0800

    update threads controller

--- app/controllers/brewery_threads_controller.rb
+++ app/controllers/brewery_threads_controller.rb
@@ -1,13 +1,60 @@
 class BreweryThreadsController < ApplicationController
-  def edit
-  end
+  before_action :authenticate_user!, only: [:author, :participant, :new, :create]
+  before_action :thread, only: [:show, :edit, :update]
+  before_action :require_admin_or_author_for_post!, only: [:edit, :update]
 
   def index
+    @brewery_threads = BreweryThread.updated_at_first.sorted.includes(:user, :brewery_state)
   end
 
-  def new
+  def author
+    @brewery_threads.where(user: current_user).sorted.includes(:user, :brewery_state)
+    render action: :index
+  end
+
+  def participant
+    @brewery_threads.includes(:user, :brewery_state).joins(:posts).where(posts: { user_id: current_user.id }).distinct(posts: :id).sorted
+    render action: :index
   end
 
   def show
+    @post = Post.new
+    @post.user = current_user
   end
+
+  def new
+    @brewery_thread.new
+    @brewery_thread.posts.new
+  end
+
+  def create
+    @brewery_thread = current_user.brewery_threads.new(brewery_thread_params)
+    @brewery_thread.posts.each { |post| post.user_id = current_user.id }
+    if @brewery_thread.save
+      redirect_to brewery_thread_path(@brewery_thread)
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @brewery_thread.update(brewery_thread_params)
+      redirect_to brewery_thread_path(@brewery_thread), notice: "Thread Updated Successfully."
+    else
+      render action: :edit
+    end
+  end
+
+  private
+
+    def thread
+      @brewery_thread.friendly.find(params[:id])
+    end
+
+    def brewery_thread_params
+      params.require(:brewery_thread).permit(:brewery, :brewery_state_id, posts_attributes: [:body])
+    end
 end
