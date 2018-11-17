140454e55171a2a594217c133cb92025cbd0c4ab
Xplor8r <mpieschel39@gmail.com>
2018-11-16 18:31:22 -0800

    add helper methods

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,16 @@ module PostsHelper
     def brewery_state_link(state)
        link_to state.name, brewery_state_brewery_threads_path(state)
     end
+
+    def post_classes(post)
+        klasses = ["post", "card", "mb-3"]
+        klasses << "thread-author" if post.user == @brewery_thread.user
+        klasses
+    end
+    
+    def user_badge(user)
+        if user.respond_to?(:admin) && user.admin?
+          content_tag :span, "Admin", class: "badge badge-default"
+        end
+    end
 end
