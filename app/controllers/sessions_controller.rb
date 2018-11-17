0787d4e19f91ffa6a4ce073697edf9526da00175
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-17 10:01:52 -0800

    add google login option

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,19 +9,34 @@ class SessionsController < ApplicationController
       @user = User.new
       @users = User.all
     end
-  
+
     def create
-      if @user = User.find_by(name: params[:user][:name])
-        if @user && @user.authenticate(params[:user][:password])
+      if @user = User.find_by(email: params[:email])
+        if @user && @user.authenticate(params[:password])
           session[:user_id] = @user.id
           redirect_to user_path(@user)
         else
-          redirect_to signin_path
+          redirect_to new_user_url
         end
       else
         @user = User.new
-        redirect_to signin_path
+        redirect_to new_user_url
       end  
     end
-  
+    
+    def google
+        auth = request.env['omniauth.auth']
+        if auth
+          @user = User.find_or_create_by(uid: auth['uid']) do |user|
+            user.name = auth['info']['name']
+            user.email = auth['info']['email']
+            user.password = params[:code][0..71]
+          end
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          @user = User.new
+          redirect_to new_user_url
+        end
+      end
   end
