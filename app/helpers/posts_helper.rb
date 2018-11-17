dcd0c8cc7226d3ffdeeb755baa85d3b9589e147d
Xplor8r <mpieschel39@gmail.com>
2018-11-17 15:45:42 -0800

    update posts helper views css

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,5 @@
 module PostsHelper
-    def avatar_tag(email)
-       gravatar_image_tag(email, gravatar: { size: 40 }, class: "rounded avatar")
-    end
-    
+   
     def brewery_state_link(state)
        link_to state.name, brewery_state_brewery_threads_path(state)
     end
