f142800b6136028600aad495ee758e48c239d4ac
Michael Pieschel <mpieschel39@gmail.com>
2018-11-18 14:29:24 -0800

    Update sessions_controller.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,30 +1,25 @@
 class SessionsController < ApplicationController
 
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.find_by(name: params[:user][:name])
+        if @user && @user.authenticate(params[:user][:password])
+          session[:user_id] = @user.id
+          redirect_to user_path(@user)
+        else
+          flash[:message] = "No one here by that name. Please sign up."
+          redirect_to signup_path
+        end
+    end
+    
     def destroy
       session[:user_id] = nil
       redirect_to root_path
     end
   
-    def new
-      @user = User.new
-      @users = User.all
-    end
-
-    def create
-      if @user = User.find_by(email: params[:email])
-        if @user && @user.authenticate(params[:password])
-          session[:user_id] = @user.id
-          redirect_to user_path(@user)
-        else
-          flash[:message] = "No one here by that name. Please sign up."
-          redirect_to new_user_url
-        end
-      else
-        @user = User.new
-        redirect_to new_user_url
-      end  
-    end
-
     def auth
       request.env["omniauth.auth"]
     end
