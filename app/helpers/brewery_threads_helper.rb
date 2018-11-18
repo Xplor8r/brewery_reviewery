6f41aa7283853543f7703bdeb24444a208005e32
Xplor8r <mpieschel39@gmail.com>
2018-11-18 10:08:56 -0800

    more seed working on active link

--- app/helpers/brewery_threads_helper.rb
+++ app/helpers/brewery_threads_helper.rb
@@ -1,2 +1,14 @@
 module BreweryThreadsHelper
+    def brewery_link_to(path, opts={}, &block)
+        link_to path, class: brewery_link_class(path, opts), &block
+    end
+
+    def brewery_link_class(matches, opts={})
+        case matches
+        when Array
+            "active" if matches.any?{ |m| request.path.starts_with?(m) }
+        when String
+            "active" if opts.fetch(:exact, false) ? request.path == matches : request.path.starts_with?(matches)
+        end
+    end
 end
