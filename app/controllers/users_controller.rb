17df53220de399e006eeb6f9c213ca715845434e
Michael Pieschel <mpieschel39@gmail.com>
2018-11-18 14:17:37 -0800

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     if @user.valid?
       @user.save
       session[:user_id] = @user.id
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       flash[:message] = "You must give a valid name and a unique email."
       redirect_to new_user_path
