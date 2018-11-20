f306518a9f7a262054acfc638b3add9fa50688ab
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-20 05:32:04 +0000

    add friendly id to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     has_many :brewery_threads
     has_many :posts
-    has_many :posts, through: :brewery_threads
     has_secure_password
-
+    extend FriendlyId
+    friendly_id :name, use: :slugged
 end
--- db/migrate/20181114222919_create_users.rb
+++ db/migrate/20181114222919_create_users.rb
@@ -2,8 +2,8 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :name
+      t.string :slug, null: false
       t.string :email
-      t.string :uid
       t.string :password_digest
       t.boolean :admin, default: false
 
