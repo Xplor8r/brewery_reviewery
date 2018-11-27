df75c1f831461fc56b5ba636981713be2392b003
Xplor8r <mpieschel39@gmail.com>
2018-11-26 17:18:06 -0800

    update state and user model

--- app/models/brewery_state.rb
+++ app/models/brewery_state.rb
@@ -1,7 +1,8 @@
 class BreweryState < ApplicationRecord
     extend FriendlyId
-    has_many :brewery_threads
     friendly_id :name, use: :slugged
+    has_many :brewery_threads
+
     scope :sorted, ->{order(name: :asc)}
     validates :name, :slug, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-    has_many :brewery_threads
-    has_many :posts
-    has_secure_password
     extend FriendlyId
     friendly_id :name, use: :slugged
+    has_many :brewery_threads
+    has_many :posts
+    has_many :posts, through: :brewery_threads
+
+    has_secure_password
+
 end
