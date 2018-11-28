c0a8a149e2b92ac5dd11ecf30a1663128ed33c19
Michael Pieschel <mpieschel39@gmail.com>
2018-11-27 16:53:52 -0800

    has many users through brewery threads

--- app/models/brewery_state.rb
+++ app/models/brewery_state.rb
@@ -2,7 +2,8 @@ class BreweryState < ApplicationRecord
     extend FriendlyId
     friendly_id :name, use: :slugged
     has_many :brewery_threads
-
+    has_many :users, through: :brewery_threads
+    
     scope :sorted, ->{order(name: :asc)}
     validates :name, :slug, presence: true
 end
