9d5a3720b3126a147635ca8d4cf4694f0c07c4ec
Michael Pieschel <mpieschel39@gmail.com>
2018-11-27 16:49:03 -0800

    has many brewery states through brewery threads

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     friendly_id :name, use: :slugged
     has_many :brewery_threads
     has_many :posts
+    has_many :brewery_states, through: :brewery_threads
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true
 
