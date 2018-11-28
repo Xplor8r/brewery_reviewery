3919009904e5508d394472b483b181206ae61b77
Michael Pieschel <mpieschel39@gmail.com>
2018-11-27 16:36:36 -0800

    update validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
     friendly_id :name, use: :slugged
     has_many :brewery_threads
     has_many :posts
-    has_many :posts, through: :brewery_threads
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
 
     has_secure_password
 
