86c7450bcaf59eacb2934be502f5b8006c7b1fa6
Xplor8r <mpieschel39@gmail.com>
2018-12-16 22:14:05 -0800

    fix read more link bug

--- app/assets/javascripts/brewery_threads.js
+++ app/assets/javascripts/brewery_threads.js
@@ -1,17 +1,19 @@
 $( document ).on('turbolinks:load', function() {
-    console.log('document loaded');
     attachListeners();
 });
 
 
 function attachListeners() {
+    // show more of brewery_thread text
     $(".js-more").on("click", function(e) {
         e.preventDefault();
         var id = parseInt($(".js-more").attr("data-id"));
         $.get("/threads/" + id + ".json", function(data) {
             let postBody = data["posts"][0]["body"]
-            console.log(postBody);
+            $("#brewery_thread-" + id).html(postBody);
         }); 
     });
+
+
 }
 
