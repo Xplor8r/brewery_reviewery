09c5a5f5e545d1945c0a37b397e7fb98e5668307
Xplor8r <mpieschel39@gmail.com>
2018-12-17 20:09:30 -0800

    fix serializer bug

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,5 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :id, :body, :brewery_thread_id, :created_at
-  belongs_to :user, serializer: PostUserSerializer
+  attributes :id, :body, :user, :brewery_thread_id, :created_at
+  belongs_to :user
   belongs_to :brewery_thread
 end
--- app/serializers/post_user_serializer.rb
+++
@@ -1,3 +0,0 @@
-class PostUserSerializer < ActiveModel::Serializer
-    attributes :name
-  end
