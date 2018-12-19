8499b7df62ee1a2e90d9bdfdceda76edc49e4813
Xplor8r <mpieschel39@gmail.com>
2018-12-19 12:49:50 -0800

    move next and previous links format date install packag.json

--- app/assets/javascripts/brewery_threads.js
+++ app/assets/javascripts/brewery_threads.js
@@ -25,7 +25,7 @@ function attachListeners() {
         $.get("/threads/" + nextId + ".json", function(data) {
 
             const stateId = new State(data["brewery_state"]).jsFriendlyId();
-            console.log(stateId)
+            console.log(data["created_at"])
             const userId = new User(data["user"]).jsFriendlyId();
             $(".thread-header").html(data["brewery"]);
             $(".thread-state").html('<a href="/threads/brewery_state/' + stateId + '"' + ">" + data["brewery_state"]["name"] + "</a>");
@@ -45,15 +45,15 @@ function attachListeners() {
             const stateId = new State(data["brewery_state"]).jsFriendlyId();
             const userId = new User(data["user"]).jsFriendlyId();
             const posts = data["posts"];
-
+            console.log(timeago.format(data["created_at"]))
             $(".thread-header").html(data["brewery"]);
             $(".thread-state").html('<a href="/threads/brewery_state/' + stateId + '"' + ">" + data["brewery_state"]["name"] + "</a>");
             $(".thread-created").html(data["created_at"]);
             $(".thread-user").html('<a href="/users/' + userId + '"' + ">" + data["user"]["name"] + "</a>");
-            posts.forEach(function(attributes){
-                const comment = new Post(attributes);
-                comment.show();
-            })
+            // posts.forEach(function(attributes){
+            //     const comment = new Post(attributes);
+            //     comment.show();
+            // })
             $(".js-next").attr("data-id", data["id"]);
             $(".js-previous").attr("data-id", data["id"]);
         }); 
@@ -68,7 +68,6 @@ function attachListeners() {
         }).done(function(data){
             $("#brewery_thread-" + data["id"]).html(data["posts"][0]["body"])
             data["posts"].shift();
-            console.log(data["posts"])
             const posts = data["posts"];
             posts.forEach(function(attributes){
                 const comment = new Post(attributes);
@@ -77,3 +76,21 @@ function attachListeners() {
         })
     })
 }
+import TimeAgo from 'javascript-time-ago'
+ 
+// Load locale-specific relative date/time formatting rules.
+import en from 'javascript-time-ago/locale/en'
+
+TimeAgo.addLocale(en)
+const timeAgo = new TimeAgo('en-US')
+timeAgo.format(new Date())
+// "just now"
+ 
+timeAgo.format(Date.now() - 60 * 1000)
+// "a minute ago"
+ 
+timeAgo.format(Date.now() - 2 * 60 * 60 * 1000)
+// "2 hours ago"
+ 
+timeAgo.format(Date.now() - 24 * 60 * 60 * 1000)
+// "a day ago"
--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,9 +1,11 @@
 class Post {
     constructor(attributes) {
+
         this.body = attributes["body"];
         this.userId = attributes["user"]["id"];
         this.userName = attributes["user"]["name"];
-        this.created = attributes["created_at"];
+        this.created = new Date(attributes["created_at"]).format();
+        console.log(this.created)
         this.threadId = attributes["brewery_thread_id"];
     }
 
@@ -11,11 +13,23 @@ class Post {
 
         var $div = $(".show-comments-" + this.threadId)
         $div.append(
-                '<div class="card-header"><div class="post-user"><small><strong>' +
-                '<a href="/users/' + this.userId + '"' + ">" + this.userName + "</a></strong></small>" + 
+                '<div class="card-header"><div class="post-user"><small>' +
+                '<a href="/users/' + this.userId + '"' + '>' + this.userName + '</a></small>' + 
                 '<small class="post-created"> commented on ' + 
                 this.created + '</small></div></div><div class="card-body"><p class="text-muted post-body">' +
                 this.body + '</p></div>'
             );
     }
+}
+
+Date.prototype.format = function() {
+    var month_names =["Jan","Feb","Mar",
+    "Apr","May","Jun",
+    "Jul","Aug","Sep",
+    "Oct","Nov","Dec"];   
+    var day = this.getDate();
+    var month_index = this.getMonth();
+    var year = this.getFullYear();
+
+    return "" + month_names[month_index] + " " + day + " " + year;
 }
